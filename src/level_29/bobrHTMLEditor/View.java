e3ff1dbecf7ba1707d61be5cbbed60bf2e0e5624
Pavel <ivankov_ps@mail.ru>
2021-10-02 16:37:04 +0300

    level 29 comlete 90%

--- src/level_29/bobrHTMLEditor/View.java
+++ src/level_29/bobrHTMLEditor/View.java
@@ -2,8 +2,10 @@ package level_29.bobrHTMLEditor;
 
 import level_29.bobrHTMLEditor.listeners.FrameListener;
 import level_29.bobrHTMLEditor.listeners.TabbedPaneChangeListener;
+import level_29.bobrHTMLEditor.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +16,8 @@ public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
 
     public View() {
         try {
@@ -82,10 +86,34 @@ public class View extends JFrame implements ActionListener {
     }
 
     public boolean canUndo() {
-        return false;
+        return undoManager.canUndo();
     }
 
     public boolean canRedo() {
-        return false;
+        return undoManager.canRedo();
+    }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
     }
 }
